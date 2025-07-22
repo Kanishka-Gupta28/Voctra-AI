166d3aca225ff35d215f8cfbd88c8ce8b39aec39
Kanishka Gupta <https://github.com/Kanishka-Gupta28>
2025-07-22 14:04:22 +0530

    first commit

---
+++ MusicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://youtu.be/Q3JMD4oaXlI?si=YMIPyUiHHXk5YONm",
+    "Saiyaara": "https://youtu.be/BSJa1UytM8w?si=1j-MWzo48xIwy6Yr",
+    "bad": "https://youtu.be/K-Ts-NFR62o?si=BK3W2QrHSZjQE0i8",
+    "wolf": "https://youtu.be/mOEL8Q-2bSo?si=w_JfFeaALkoDBGa1"
+}
